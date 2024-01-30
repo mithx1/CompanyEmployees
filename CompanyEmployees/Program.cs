34ff7b464e4ca46c1662ef273ad2567f8740b404
Julius <71910804+mithx1@users.noreply.github.com>
2024-01-31 07:29:33 +0800

    Changes:
    1. Two new methods, `ConfigureCors` and `ConfigureIISIntegration`, have been added to the `ServiceExtensions` class in the `CompanyEmployees.Extensions` namespace. The `ConfigureCors` method allows any origin, method, and header, while the `ConfigureIISIntegration` method is a placeholder for future IIS configurations.
    
    2. The `CompanyEmployees.Extensions` and `Microsoft.AspNetCore.HttpOverrides` namespaces have been imported in the `Program.cs` file. The former is used to call the new extension methods, while the latter provides types and middleware for managing HTTP request overrides.
    
    3. The `ConfigureCors` and `ConfigureIISIntegration` methods are called on the `builder.Services` object in the `Program.cs` file. This adds the CORS and IIS configurations to the service container.
    
    4. The `UseStaticFiles` middleware has been added to the HTTP request pipeline in the `Program.cs` file. This enables the serving of static files.
    
    5. The `UseForwardedHeaders` middleware has been added to the HTTP request pipeline in the `Program.cs` file with a configuration to forward all headers. This is typically used when an application is hosted behind a proxy server or load balancer.
    
    6. The `UseHsts` middleware is now only added in non-development environments in the `Program.cs` file. This middleware adds the Strict-Transport-Security header to HTTP responses, enforcing the use of HTTPS.

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,14 +1,34 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else 
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
 
 app.UseAuthorization();
 
