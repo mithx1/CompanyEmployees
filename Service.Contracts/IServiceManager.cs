e1a4d2a09eee75eb815a681ed38379e85dc51a2d
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-04 18:48:45 +0800

    The most important changes include the addition of a new project named "Service" to the solution file "CompanyEmployees.sln", the addition of two new properties to the "IServiceManager" interface, and the creation of a new class "CompanyService". A new project file "Service.csproj" has also been created for the "Service" project.
    List of Changes:
    
    1. A new project named "Service" has been added to the solution file "CompanyEmployees.sln". This project has been added to the solution configurations for both Debug and Release modes for the Any CPU platform. The project's GUID is "{1307A31F-72E8-4FA0-8ACD-21A895F2544F}" and the project file is "Service\Service.csproj". (Reference: CompanyEmployees.sln)
    
    2. Two new properties, "CompanyService" and "EmployeeService", have been added to the "IServiceManager" interface in the "Service.Contracts" namespace. This change was made in the file "IServiceManager.cs". (Reference: IServiceManager.cs)
    
    3. A new class "CompanyService" has been added in the "Service" namespace. This class implements the "ICompanyService" interface and has two private readonly fields "_repository" and "_logger". The constructor of this class takes two parameters of types "IRepositoryManager" and "ILoggerManager" and assigns them to the respective fields. This change was made in the file "CompanyService.cs". (Reference: CompanyService.cs)
    
    4. A new project file "Service.csproj" has been created for the "Service" project. This project targets the .NET 6.0 framework, enables implicit usings and nullable reference types. It also references two other projects: "Contracts" and "Service.Contracts". (Reference: Service\Service.csproj)

--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -8,5 +8,7 @@ namespace Service.Contracts
 {
     public interface IServiceManager
     {
+        ICompanyService CompanyService { get; }
+        IEmployeeService EmployeeService { get; }
     }
 }
