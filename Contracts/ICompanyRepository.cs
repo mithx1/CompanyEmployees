746a694cd9b372e2eb45e2ff8a720f29475457cf
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-05 00:27:22 +0800

    -The `Contracts.csproj` file now includes a reference to the `Entities.csproj` project, enabling the use of classes and types from the `Entities` project in the `Contracts` project.
    -A new method `GetAllCompanies(bool trackChanges)` has been added to the `ICompanyRepository` interface. This method returns an `IEnumerable` of `Company` objects, with the `trackChanges` parameter likely determining whether changes to the returned `Company` objects should be tracked.

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Models;
+
 
 namespace Contracts
 {
     public interface ICompanyRepository
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
