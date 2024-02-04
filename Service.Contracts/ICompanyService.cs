93f86ff45f1ca8f5d8135a56e33e43d21d52e869
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-05 00:25:50 +0800

    Addition of a new method: A new method `GetAllCompanies(bool trackChanges)` that returns an `IEnumerable` of `Company` objects has been added to the `ICompanyService` interface. This new method suggests an enhancement in the functionality of the `ICompanyService` interface, allowing it to retrieve a list of all companies, with an option to track changes.

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Models;
 
 namespace Service.Contracts
 {
     public interface ICompanyService
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
