8f5ce642e7b4fd9be2413e6fbe81a60932743062
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-05 23:50:38 +0800

    implement a dto fix project references to use the dto project

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -16,13 +17,18 @@ namespace Service
             _logger = logger;
         }
 
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try 
             {
                 var companies = _repository.Company
                     .GetAllCompanies(trackChanges);
-                return companies;
+
+                var companiesDto = companies.Select(c =>
+                new CompanyDto(c.Id, c.Name ?? "", string.Join(' ',
+                c.Address, c.Country)))
+                    .ToList();
+                return companiesDto;
             }
             catch (Exception ex)
             {
