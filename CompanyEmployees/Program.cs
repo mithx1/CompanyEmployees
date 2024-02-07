e0ff3eacfb0a4896680c4369fa606952f83d6e8b
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-07 23:48:28 +0800

    configure ExceptionMiddleware

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 
@@ -22,15 +23,11 @@ builder.Services.AddControllers()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if(app.Environment.IsProduction()) 
     app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -38,6 +35,8 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.All
 });
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
