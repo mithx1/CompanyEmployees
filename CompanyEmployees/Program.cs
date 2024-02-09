f8d72a9ce8cfe1c3229632b576c7cfae03ea855d
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-09 13:19:34 +0800

    update accept header

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -20,7 +20,8 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers(config =>
 { 
-    config.RespectBrowserAcceptHeader = true; 
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters();
 
 builder.Services.AddControllers()
