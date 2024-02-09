597312b9ccc0c263dae679a4aa9212b4f06e9ab7
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-09 11:55:50 +0800

    service level interface and implemenation

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Entities.Models;
+using Shared.DataTransferObjects;
 
 namespace Service.Contracts
 {
     public interface IEmployeeService
     {
+       IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
