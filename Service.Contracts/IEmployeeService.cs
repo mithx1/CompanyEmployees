fe6b4cfd087637bb57644658ff233379f7657902
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-09 12:09:39 +0800

    service interface and implementation

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -8,5 +8,6 @@ namespace Service.Contracts
     public interface IEmployeeService
     {
        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+        EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
     }
 }
