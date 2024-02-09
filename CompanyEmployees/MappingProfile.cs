c0ca19c109256a9fa0e6337367ed7358be421981
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-09 13:19:19 +0800

    update mapping profiles

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -9,9 +9,8 @@ namespace CompanyEmployees
         public MappingProfile()
         {
             CreateMap<Company, CompanyDto>()
-                .ForCtorParam("FullAddress",
-                opt => opt.MapFrom(x =>
-                string.Join(' ', x.Address, x.Country)));
+                .ForMember(c => c.FullAddress,
+                opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
         }
