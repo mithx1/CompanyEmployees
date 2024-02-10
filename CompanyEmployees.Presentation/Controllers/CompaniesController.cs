27902a93e4b2d6ba4d0e3bb05afca408a0a11476
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-11 07:54:26 +0800

    modelbinding for collection of GetCompanies

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 
@@ -42,7 +43,8 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        public IActionResult GetCompanyCollection(
+            [ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
             var companies =
                 _service.CompanyService.GetByIds(ids, trackChanges: false);
