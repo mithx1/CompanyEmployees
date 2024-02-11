11a5a9614ceb54a6d6ccc5dd8dfdd80d5202ebd1
Julius <71910804+mithx1@users.noreply.github.com>
2024-02-11 13:46:36 +0800

    udpate employee (Disconnected update)

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -15,5 +15,12 @@ namespace Service.Contracts
             EmployeeForCreationDto employeeForCreation, bool trackChanges);
 
         void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges);
+
+        void UpdateEmployeeForCompany(
+            Guid companyId, 
+            Guid id,
+            EmployeeForUpdateDto employeeForUpdate, 
+            bool compTrackChanges, 
+            bool empTrackChanges);
     }
 }
---
+++ Shared/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Shared.DataTransferObjects
+{
+    public record EmployeeForUpdateDto(string Name, int Age, string Position);
+    
+}
